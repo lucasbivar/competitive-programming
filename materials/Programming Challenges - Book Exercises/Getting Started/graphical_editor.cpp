f9a302b29c681421c80ce128190242ad41b16377
Lucas Bivar <lucasbivarfonseca@gmail.com>
2021-02-05 15:13:12 -0300

    refactoring graphical editor

--- materials/Programming Challenges - Book Exercises/Getting Started/graphical_editor.cpp
+++ materials/Programming Challenges - Book Exercises/Getting Started/graphical_editor.cpp
@@ -1,112 +1,126 @@
-#include <iostream>
-#include <string>
-#include <cstring>
+#include <bits/stdc++.h>
+#define MAX 255
 
 using namespace std;
 
-char matriz[260][260];
-int visited[260][260];
 int movX[4] = {0, 1, 0, -1};
 int movY[4] = {-1, 0, 1, 0};
-char f, color, inicial;
-int x1, y1, x2, y2;
-int size_x, size_y;
-string name;
+int visited[MAX][MAX];
+char matrix[MAX][MAX];
+char beginnerColor;
 
-void create_table(){
-    for(int i = 0; i < size_y; i++){
-        for(int j = 0; j < size_x; j++){
-            matriz[i][j] = 'O';
-        }
-    }
-}
-
-void print_table(){
-    cout << name << endl;
-    for(int i = 0; i < size_y; i++){
-        for(int j = 0; j < size_x; j++){
-            cout << matriz[i][j];
-        }
-        cout << endl;
-    }
-}
-
-void draw_vertical(){
-    for(int i = y1; i <= y2; i++){
-        matriz[i][x1] = color;
-    }
-}
-
-void draw_horizontal(){
-    for(int i = x1; i <= x2; i++){
-        matriz[y1][i] = color;
-    }
-}
-
-void fill_rectangle(){
-    for(int i = y1; i <= y2; i++){
-        for(int j = x1; j <= x2; j++){
-            matriz[i][j] = color;
-        }
-    }
-}
-
-void fill_region(int x0, int y0, char color){
-    matriz[y0][x0] = color;
-    visited[y0][x0] = 1;
-    for(int i = 0; i < 4; i++){
-        int new_x = x0 + movX[i], new_y = y0 + movY[i];
-        if(new_x < 0 || new_x < 0 || new_x > (size_x -1) || new_y > (size_y - 1)) continue;
-        if(matriz[new_y][new_x] != inicial || visited[new_y][new_x] != 0 ) continue;
-        fill_region(new_x, new_y, color);
-    }
-}
+void printTable(int n, int m, string fileName);
+void fillRectangle(int x_1, int y_1, int x_2, int y_2, char color);
+void horizontalSegment(int x_1, int x_2, int y_1, char color);
+void verticalSegment(int x_1, int y_1, int y_2, char color);
+void fillRegion(int x_1, int y_1, int n, int m, char color);
 
 int main(){
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ios::sync_with_stdio(0);
-    
-    while (true){
-        cin >> f;
-        if(f == 'X') break;
-        if(f == 'I'){
-            cin >> size_x >> size_y;
-            create_table();
-        }else if(f == 'C'){
-            create_table();
-        }else if(f == 'S'){
-            cin.ignore();
-            getline(cin, name);
-            print_table();
-        }else if(f == 'L'){
-            cin >> x1 >> y1 >> color ;
-            matriz[y1-1][x1-1] = color;
-        }else if(f == 'V'){
-            cin >> x1 >> y1 >> y2 >> color;
-            x1--;y1--;y2--;
-            if(y2 < y1) swap(y1, y2);
-            draw_vertical();
-        }else if(f == 'H'){
-            cin >> x1 >> x2 >> y1 >> color;
-            x1--;x2--;y1--;
-            if(x2 < x1) swap(x1, x2);
-            draw_horizontal();
-        }else if(f == 'K'){
-            cin >> x1 >> y1 >> x2 >> y2 >> color;
-            x1--; x2--;y1--;y2--;
-            if(y2 < y1) swap(y1, y2);
-            if(x2 < x1) swap(x1, x2);
-            fill_rectangle();
-        }else if(f == 'F'){
-            cin >> x1 >> y1 >> color;
-            x1--; y1--;
-            memset(visited, 0, sizeof(visited));
-            inicial = matriz[y1][x1];
-            fill_region(x1, y1, color);
-        }
+  cin.tie(NULL);
+  cout.tie(NULL);
+  ios::sync_with_stdio(0);
+
+  int m, n, x_1, x_2, y_1, y_2;
+  char action, color;
+  string fileName = "";
+
+  while (cin >> action){
+
+    if(action == 'I'){
+      cin >> m >> n;
+      memset(matrix,'O',sizeof(matrix));
+
+    }else if (action == 'C'){
+
+      memset(matrix,'O',sizeof(matrix));
+
+    }else if(action == 'L'){
+
+      cin >> x_1 >> y_1 >> color;
+      matrix[--y_1][--x_1] = color;
+
+    }else if(action == 'V'){
+      cin >> x_1 >> y_1 >> y_2 >> color;
+      if(y_1 > y_2) swap(y_1, y_2);
+      verticalSegment(--x_1, --y_1, --y_2, color);
+
+    }else if(action == 'H'){
+
+      cin >> x_1 >> x_2 >> y_1 >> color;
+      if(x_1 > x_2) swap(x_1, x_2);
+
+      horizontalSegment(--x_1, --x_2, --y_1, color);
+
+    }else if(action == 'K'){
+
+      cin >> x_1 >> y_1 >> x_2 >> y_2 >> color;
+      if(y_2 < y_1) swap(y_2, y_1);
+      if(x_2 < x_1) swap(x_2, x_1);
+
+      fillRectangle(--x_1, --y_1, --x_2, --y_2, color);
+
+    }else if(action == 'F'){
+
+      cin >> x_1 >> y_1 >> color;
+      memset(visited, 0, sizeof(visited));
+      --x_1;--y_1;
+      beginnerColor = matrix[y_1][x_1];
+      fillRegion(x_1, y_1, n, m, color);
+
+    }else if(action == 'S'){
+      cin.ignore();
+      getline(cin, fileName);
+
+      printTable(n, m, fileName);
+
+    }else if(action == 'X'){
+      break;
     }
-    
-    
-    return 0;
+
+  }
+  
+  return 0;
+}
+
+
+void printTable(int n, int m, string fileName){
+  cout << fileName << endl;
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < m; j++){
+      cout << matrix[i][j];
+    }
+    cout << endl;
+  }
+}
+
+void fillRectangle(int x_1, int y_1, int x_2, int y_2, char color){
+  for(int i = y_1; i <= y_2; i++){
+    for(int j = x_1; j <= x_2; j++){
+      matrix[i][j] = color;
+    }
+  }
+}
+
+void horizontalSegment(int x_1, int x_2, int y_1, char color){
+  for(int j = x_1; j <= x_2; j++){
+    matrix[y_1][j] = color;
+  }
+}
+
+void verticalSegment(int x_1, int y_1, int y_2, char color){
+  for(int i = y_1; i<= y_2; i++){
+    matrix[i][x_1] = color;
+  }
+}
+
+void fillRegion(int x_1, int y_1, int n, int m, char color){
+  matrix[y_1][x_1] = color;
+  visited[y_1][x_1] = 1;
+
+  for(int k = 0; k < 4; k++){
+    int newX = x_1 + movX[k], newY = y_1 + movY[k];
+    if(newX < 0 || newY < 0 || newX > (m -1) || newY > (n - 1)) continue;
+    if(matrix[newY][newX] != beginnerColor || visited[newY][newX] != 0 ) continue;
+    fillRegion(newX, newY, n, m, color);
+  }
 }
