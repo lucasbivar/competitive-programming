fa5e1e357e9c5b94d098433852aa1351530098be
Lucas Bivar <lucasbivarfonseca@gmail.com>
2021-05-28 22:17:48 -0300

    adding cp template

---
+++ template_cp.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define f first
+#define s second
+#define sz size
+#define pb push_back
+#define mp make_pair
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+#define SQ(a) (a)*(a)
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+
+int main(){
+  cin.tie(NULL);
+  cout.tie(NULL);
+  ios::sync_with_stdio(0);
+
+  return 0;
+}
+
+// g++ -Wall -std=c++17 problem.cpp -o main && ./main < in
