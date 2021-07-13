b5d0c549dc737ffbb2d9950fe9cadbdc326bc26c
lucasbivar <lucasbivarfonseca@gmail.com>
2021-07-13 01:20:35 -0300

    feat: modifying template

--- template_cp.cpp
+++ template_cp.cpp
@@ -7,6 +7,7 @@
 #define REP(i,a,b) for (int i = a; i <= b; i++)
 #define SQ(a) (a)*(a)
 using namespace std;
+#define endl '\n'
 typedef long long ll;
 typedef vector<int> vi;
 typedef pair<int, int> pii;
